e98bc9a1d79a93c194fcb0d97f7219e70e8271be
PremBalani <balaniprem27@gmail.com>
2023-12-16 12:54:05 -0500

    Initial Commit

---
+++ SkillSync/SkillSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSyncApp.swift
+//  SkillSync
+//
+//  Created by Prem Balani on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SkillSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
